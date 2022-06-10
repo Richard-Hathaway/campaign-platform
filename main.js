fd999b411dc216549381c2b4118514eb136e756d
Richard Hathaway <rdhath@gmail.com>
2022-06-09 23:15:11 -0400

    start random lists for message generation

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log("main.js loaded successfully");
+const mission = document.getElementById("mission");
+const firstNames = ["Chandrakanta", "Sylva", "Polat", "Adrian", "Yaen", "Anton", "Timo", "Goranka", "Heimdall", "Helena", "Beth", "Rupa", "Alfred"];
+const lastNames = ["Stephens", "Espenson", "Confortola", "Beiler", "Caivano", "Rimmer", "Blakeslee", "Larson", "Agramunt", "Cuéllar", "Meggyesfalvi"];
+const parties = ["Dixiecrat Party", "Socialist Part of Minnesota", "Jeff Party", "Party Party"];
+const buzzwords = ["hope", "change", "peace", "market shares", "saving face"];
+const taxes = ["cereal", "lottery winnings", "live animal export", "cigarette"];
+const issues = ["truancy", "diploma fraud", "belt-way insiders", ""];
+
+
+mission.textContent = "test";
